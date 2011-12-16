faa586fb3dfb50a5d6da02a84e1848cfe94ca9fd
Tim McGilchrist <timmcgil@gmail.com>
2011-12-17 10:18:28 +1100

    Add the slide_machine_core application.
    It mediates between the backend DB system and the webmachine based REST interface.

---
+++ apps/slide_machine_core/src/slide_machine_core.erl
@@ -0,0 +1,25 @@
+-module(slide_machine_core).
+-author('Tim McGilchrist <timmcgil@gmail.com>').
+-export([start/0, start_link/0, stop/0]).
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
+
+
+start_link() ->
+    ensure_started(crypto),
+    slide_machine_core_sup:start_link().
+
+start() ->
+    ensure_started(crypto),
+    application:start(slide_machine_core).
+
+stop() ->
+    Res = application:stop(slide_machine_core),
+    application:stop(crypto),
+    Res.
