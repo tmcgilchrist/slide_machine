ec792895c544e89fd7d63a67ad0b452c8be8e4a9
Tim McGilchrist <timmcgil@gmail.com>
2012-02-17 07:30:01 +1100

    Add ErlDTL as the templating language and render that out for GET /slide

--- apps/slide_machine_web/src/sm_resource_slide.erl
+++ apps/slide_machine_web/src/sm_resource_slide.erl
@@ -14,10 +14,11 @@ init([]) ->
 allowed_methods(ReqData, Context) ->
     {['HEAD', 'GET'], ReqData, Context}.
 
-to_html(ReqData, Context) ->
-    Path = wrq:path_info(id, ReqData),
-    Html = io_lib:format("<html><body><h1>slide</h1><h2>Introduction</h2><p>Things</p><p>~s</p></body></html>", [Path]),
-    {Html, ReqData, Context}.
+to_html(ReqData, State) ->
+   Path = wrq:path_info(id, ReqData),
+
+    {ok, Content} = slide_dtl:render([{param, Path}]),
+    {Content, ReqData, State}.
 
 to_json(ReqData, Context) ->
     JsonDoc = "{ slide : {
