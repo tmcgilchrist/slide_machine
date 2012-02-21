73835e5916700b162ae06a1b62325449ee341870
Tim McGilchrist <timmcgil@gmail.com>
2012-02-22 08:12:14 +1100

    Serve up the index.dtl template on requests to the root url.

--- apps/slide_machine_web/src/slide_machine_web_resource.erl
+++ apps/slide_machine_web/src/slide_machine_web_resource.erl
@@ -1,9 +1,15 @@
 -module(slide_machine_web_resource).
--export([init/1, to_html/2]).
+-export([init/1,
+         allowed_methods/2,
+         to_html/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init([]) -> {ok, undefined}.
 
+allowed_methods(ReqData, State) ->
+    {['HEAD', 'GET'], ReqData, State}.
+
 to_html(ReqData, State) ->
-    {"<html><body>Hello, new world</body></html>", ReqData, State}.
+    {ok, Content} = index_dtl:render([]),
+    {Content, ReqData, State}.
