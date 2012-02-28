59b58d80e3e53760c9a23f361f8c89153263509f
Tim McGilchrist <timmcgil@gmail.com>
2012-02-29 08:23:12 +1100

    Add deck/[:key] and decks resources.

---
+++ apps/slide_machine_web/src/sm_resource_decks.erl
@@ -0,0 +1,32 @@
+-module(sm_resource_decks).
+-export([init/1,
+         allowed_methods/2,
+         to_html/2,
+         to_json/2]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+
+-record(context, {}).
+
+init([]) ->
+    {ok, #context{}}.
+
+allowed_methods(ReqData, State) ->
+    {['HEAD', 'GET'], ReqData, State}.
+
+
+to_html(ReqData, State) ->
+    to_json(ReqData, State).
+
+to_json(ReqData, State) ->
+    JsonDoc = "[{ \"title\": \"Webmachine Rocks!\",
+                  \"author\": \"Basho\",
+                  \"summary\": \"Webmachine is a toolkit for building RESTful web applications.\",
+                  \"slides\": []
+                },{
+                  \"title\": \"Jasmine for the testing\",
+                  \"author\": \"Pivotal Labs\",
+                  \"summary\": \"Jasmine is a TDD style framework influenced by rspec.\",
+                  \"slides\": []
+                }]",
+    {JsonDoc, ReqData, State}.
