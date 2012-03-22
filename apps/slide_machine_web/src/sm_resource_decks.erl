6268979bec820d602b1e78403d6007893718aa71
Tim McGilchrist <timmcgil@gmail.com>
2012-03-23 07:33:10 +1100

    Merge the deck resources and change the URL structure to be like rails.

--- apps/slide_machine_web/src/sm_resource_decks.erl
+++ apps/slide_machine_web/src/sm_resource_decks.erl
@@ -1,41 +1,45 @@
 -module(sm_resource_decks).
 -export([init/1,
          allowed_methods/2,
-         to_html/2,
-         to_json/2]).
+         content_types_provided/2,
+         to_json/2,
+        finish_request/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
--record(context, {}).
+-record(context, {db}).
 
 init([]) ->
-    {ok, #context{}}.
+    {ok,{}}.
 
 allowed_methods(ReqData, State) ->
     {['HEAD', 'GET'], ReqData, State}.
 
-
-to_html(ReqData, State) ->
-    to_json(ReqData, State).
+content_types_provided(ReqData, State) ->
+    {[{"application/json", to_json}], ReqData, State}.
 
 to_json(ReqData, State) ->
-    JsonDoc = "[{ \"title\": \"Webmachine Rocks!\",
-                  \"author\": \"Basho\",
-                  \"summary\": \"Webmachine is a toolkit for building RESTful web applications.\",
-                  \"slides\": [ {
-                    \"title\": \"Introducing Webmachine\",
-                    \"content\": \"Introductory material\"
-                  }, {
-                    \"title\": \"Why?\",
-                    \"content\": \"Cause Erlang deserves web frameworks.\"
-                  }, {
-                    \"title\": \"Where?\",
-                    \"content\": \"Basho\"
-                  }]
-                },{
-                  \"title\": \"Jasmine for the testing\",
-                  \"author\": \"Pivotal Labs\",
-                  \"summary\": \"Jasmine is a TDD style framework influenced by rspec.\",
-                  \"slides\": []
-                }]",
-    {JsonDoc, ReqData, State}.
+    %% Load key from arguments
+    %% Call slide_machine_core to grab the data.
+    case wrq:path_info(id, ReqData) of
+        undefined ->
+            % Return all decks
+            All = "[{ title: 'Webmachine Rocks!', author: 'Basho',
+                      summary: 'Webmachine is a toolkit for building RESTful web applications.',
+                      slides: [ { title: 'Introducing Webmachine', content: 'Introductory material' },
+                                { title: 'Why?', content: 'Cause Erlang deserves web frameworks.'   },
+                                { title: 'Where?', content: 'Basho' }] },
+                    { title: 'Jasmine for the testing', author: 'Pivotal Labs',
+                      summary: 'Jasmine is a TDD style framework influenced by rspec.',
+                      slides: []
+                    }
+                   ]",
+            {All, ReqData, State};
+        _ID ->
+            % Find a deck by id
+            JsonDoc = "[{title: 'Webmachine', author: 'George', summary: 'Webmachine plus George is unstoppable.'}]",
+            {JsonDoc, ReqData, State}
+    end.
+
+finish_request(ReqData, State) ->
+    {true, ReqData, State}.
