ddfb65dd95d1e60bf7b2348bc9dd53f36ccece3c
Tim McGilchrist <timmcgil@gmail.com>
2012-03-27 22:14:09 +1100

    Load some test data.

--- bin/load.sh
+++ bin/load.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-curl -v -X POST -d "{title: 'WebMachine', author: 'Basho', summary: 'Webmachine is a toolkit for building RESTful web applications.', slides: [{title: 'Introducing Webmachine', content: 'Introductory material'}, {title: 'Why?', content: 'Cause Erlang deserves more web frameworks.'}, {title: 'Where?', content: 'Basho'}]}" -H "Content-Type: application/json" http://127.0.0.1:8098/riak/decks
+curl -v -X POST -d "{title: 'WebMachine', author: 'Basho', summary: 'Webmachine is a toolkit for building RESTful web applications.', slides: [{title: 'Introducing Webmachine', content: 'Introductory material'}, {title: 'Why?', content: 'Cause Erlang deserves more web frameworks.'}, {title: 'Where?', content: 'Basho'}]}" -H "Content-Type: application/json" http://127.0.0.1:8098/riak/decks/webmachine
 
-curl -v -X POST -d "{title: 'Jasmine for the testing', author: 'Pivotal Labs', summary: 'Jasmine is a TDD style framework influenced by rspec', slides: [{title: 'Jasmine One', content: 'One Jasmine'}]}" -H "Content-Type: application/json" http://127.0.0.1:8098/riak/decks
+curl -v -X POST -d "{title: 'Jasmine for the testing', author: 'Pivotal Labs', summary: 'Jasmine is a TDD style framework influenced by rspec', slides: [{title: 'Jasmine One', content: 'One Jasmine'}]}" -H "Content-Type: application/json" http://127.0.0.1:8098/riak/decks/jasmine
